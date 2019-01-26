9f5d72f571746718e4396f98ca34a519e00d14d6
Thomas Künneth <kuennetht@gmail.com>
2019-01-26 20:22:22 +0100

    C# example added

---
+++ CSharp/CSharp/Listing_02.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Listing_02
+    {
+        static void Main(string[] args)
+        {
+            Task t1 = FibonacciTask(40);
+            Task t2 = FibonacciTask(45);
+            t1.Start();
+            Console.WriteLine("1");
+            t2.Start();
+            Console.WriteLine("2");
+            Task.WaitAll(new Task[] { t1, t2 });
+            Console.WriteLine("Fertig");
+        }
+
+        static Task FibonacciTask(int n)
+        {
+            return new Task(() =>
+            {
+                Console.WriteLine($"Berechne fib({n})");
+                Console.WriteLine($"fib({n}) = {Fibonacci(n)}");
+            });
+        }
+
+        static long Fibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            return Fibonacci(n - 1) +
+                    Fibonacci(n - 2);
+        }
+    }
+}
---
+++ CSharp/CSharp/Listing_03.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public class Listing_03
+    {
+        static void Main(string[] args)
+        {
+            Task t1 = FibonacciTask(40);
+            Console.WriteLine("1");
+            Task t2 = FibonacciTask(45);
+            Console.WriteLine("2");
+            Task.WaitAll(new Task[] { t1, t2 });
+            Console.WriteLine("Fertig");
+        }
+
+        static async Task FibonacciTask(int f)
+        {
+            await Task.Run(() =>
+            {
+                Console.WriteLine($"Berechne fib({f})");
+                Console.WriteLine($"fib({f}) = {Fibonacci(f)}");
+            });
+        }
+
+        static long Fibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            return Fibonacci(n - 1) +
+                    Fibonacci(n - 2);
+        }
+    }
+}
