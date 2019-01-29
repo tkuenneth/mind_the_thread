811e80103b77cf6fec511b8529c0d29ea0dd3c58
Simon Wirtz <swirtz@exactag.com>
2019-01-29 09:11:27 +0100

    made aynchrony more explicit

--- Kotlin/src/Listing_04.kt
+++ Kotlin/src/Listing_04.kt
@@ -1,27 +1,24 @@
 import kotlinx.coroutines.*
 
 fun main() = runBlocking {
-    val j1 = fibonacciCoroutine(40)
+    val j1 = async { fibonacciCoroutine(40) }
     println("1")
-    val j2 = fibonacciCoroutine(45)
+    val j2 = async { fibonacciCoroutine(45) }
     println("2")
-    j1.join()
-    j2.join()
+    println("fib(40) = ${j1.await()}")
+    println("fib(45) = ${j2.await()}")
     println("Fertig")
 }
 
-fun fibonacciCoroutine(n: Int): Job {
-    return GlobalScope.launch {
-        println("Berechne fib($n)")
-        println("fib($n) = ${fibonacci(n)}")
-    }
+suspend fun fibonacciCoroutine(n: Int) = withContext(Dispatchers.Default) {
+    println("Berechne fib($n)")
+    fibonacci(n)
 }
 
 fun fibonacci(n: Int): Int {
-    if (n <= 1) {
-        return n
+    return if (n <= 1) {
+        n
     } else {
-        return fibonacci(n - 1) +
-                fibonacci(n - 2)
+        fibonacci(n - 1) + fibonacci(n - 2)
     }
 }
