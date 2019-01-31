9558da9a65a2a483ffdf953fe6dbdc3cd120f10b
Thomas Künneth <kuennetht@gmail.com>
2019-01-31 15:32:24 +0100

    no else needed ;-)

--- Listing_01.java
+++ Listing_01.java
@@ -26,9 +26,8 @@ public class Listing_01 {
     private static long fibonacci(final int n) {
         if (n <= 1) {
             return n;
-        } else {
-            return fibonacci(n - 1) +
-                    fibonacci(n - 2);
         }
+        return fibonacci(n - 1) +
+                fibonacci(n - 2);
     }
 }
