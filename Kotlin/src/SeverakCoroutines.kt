2b29dc821c232fcbc9c73760e4e8506d634608e1
Thomas Kuenneth <kuennetht@gmail.com>
2020-01-09 12:56:27 +0100

    Create SeverakCoroutines.kt

---
+++ Kotlin/src/SeverakCoroutines.kt
@@ -0,0 +1,12 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    for (i in 1..10) {
+        launch {
+            delay(i * 1000L)
+            println("$i finished")
+        }
+    }
+}
