27a8278776c1745ef54727fcefe9f85ea8176276
Thomas Kuenneth <kuennetht@gmail.com>
2020-01-09 13:00:05 +0100

    Update SeverakCoroutines.kt

--- Kotlin/src/SeverakCoroutines.kt
+++ Kotlin/src/SeverakCoroutines.kt
@@ -2,11 +2,14 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-fun main() = runBlocking {
-    for (i in 1..10) {
-        launch {
-            delay(i * 1000L)
-            println("$i finished")
+fun main() {
+    runBlocking {
+        for (i in 1..10) {
+            launch {
+                delay(i * 1000L)
+                println("$i finished")
+            }
         }
     }
+    println("all done")
 }
