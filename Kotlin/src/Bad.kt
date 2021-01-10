650ce267527c216bcc88c488ffca53dcfcf867cc
Thomas Kuenneth <thomas@thomaskuenneth.eu>
2021-01-10 14:22:08 +0100

    Create Bad.kt

---
+++ Kotlin/src/Bad.kt
@@ -0,0 +1,28 @@
+import java.awt.BorderLayout
+import java.awt.EventQueue.invokeLater
+import java.net.URL
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JLabel
+
+fun main() {
+    invokeLater {
+        val frame = JFrame("Bad")
+        val label = JLabel("Not clicked")
+        label.horizontalAlignment = JLabel.CENTER
+        frame.contentPane.add(label, BorderLayout.CENTER)
+        val button = JButton("Click!")
+        button.addActionListener {
+            label.text = "waiting"
+            label.text =
+                    URL("http://127.0.0.1:8080/hello/10000")
+                            .openStream()
+                            .bufferedReader()
+                            .use { it.readText() }
+        }
+        frame.contentPane.add(button, BorderLayout.SOUTH)
+        frame.pack()
+        frame.setLocationRelativeTo(null)
+        frame.isVisible = true
+    }
+}
